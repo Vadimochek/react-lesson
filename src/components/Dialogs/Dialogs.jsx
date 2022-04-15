47517bf903c1e23420f46b34f33fc703af06d963
Vadimochek <56549726+Vadimochek@users.noreply.github.com>
2022-04-15 16:16:37 +0300

    Refactor data objects

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -20,18 +20,32 @@ const Message = (props) => {
 
 
 const Dialogs = (props) => {
+    let dialogs = [
+        {id: 1, name: "Lina"},
+        {id: 2, name: "Vova"},
+        {id: 3, name: "Egor"},
+        {id: 4, name: "Lena"},
+    ]
+
+    let messages = [
+        {id: 1, message: "Hi"},
+        {id: 2, message: "Where are you?"},
+        {id: 3, message: "What are you doing"},
+        {id: 4, message: "Nice"},
+    ]
+
     return (
         <div className={css.dialogs}>
             <div className={css.dialogList}>
-                <DialogItem name='Lina' id='1'/>
-                <DialogItem name='Vova' id='2'/>
+                <DialogItem name={dialogs[0].name} id={dialogs[0].id}/>
+                <DialogItem name={dialogs[1].name} id={dialogs[1].id}/>
                 <DialogItem name='Egor' id='3'/>
                 <DialogItem name='Lena' id='4'/>
             </div>
             <div className={css.messages}>
-                <Message message='Hi'/>
-                <Message message='Where are you?'/>
-                <Message message='What are you doing'/>
+                <Message message={messages[0].message}/>
+                <Message message={messages[1].message}/>
+                <Message message={messages[2].message}/>
                 <Message message='Nice'/>
                 <Message message='Nice'/>
             </div>
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -3,6 +3,14 @@ import css from './Posts.module.css';
 import Post from './Post/Post';
 
 const Posts = () => {
+
+    let posts = [
+        {id: 1, likes: 10, message: "Hi"},
+        {id: 2, likes: 99, message: "Hey, what are you doing here?"},
+        {id: 3, likes: 16, message: "What are you doing"},
+        {id: 4, likes: 1, message: "Nice"},
+    ]
+
     return (
         <div className={css.my_post}>
             <h2>My post</h2>
@@ -11,7 +19,8 @@ const Posts = () => {
                 <div><button>Add</button></div>
             </div>
             <div className={css.posts}>
-                <Post text="OMG!?" likes="10"/>
+                <Post text={posts[0].message} likes={posts[0].likes}/>
+                <Post text={posts[1].message} likes={posts[1].likes}/>
                 <Post text="Hey, what are you doing here?" likes="0"/>
                 <Post text="Very cool image" likes="999"/>
             </div>
